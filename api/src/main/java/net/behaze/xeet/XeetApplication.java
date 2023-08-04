c5fc818a1141bf822115eee27463ccbbf5f06119
João Guilherme <jgmoreir2@gmail.com>
2023-08-03 20:56:51 -0400

    first commit

---
+++ api/src/main/java/net/behaze/xeet/XeetApplication.java
@@ -0,0 +1,13 @@
+package net.behaze.xeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XeetApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/net/behaze/xeet/XeetApplicationTests.java
@@ -0,0 +1,13 @@
+package net.behaze.xeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
